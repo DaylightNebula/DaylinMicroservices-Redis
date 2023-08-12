105f8d9b4e7e9238d5e09f7d29d379ff34255d78
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2023-08-12 00:01:11 -0600

    added publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.21"
+    `maven-publish`
+    java
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "io.github.daylightnebula"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -36,5 +38,51 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+
+            pom {
+                url.set("https://github.com/DaylightNebula/DaylinMicroservices-Redis")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("DaylightNebula")
+                        name.set("Noah Shaw")
+                        email.set("noah.w.shaw@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/DaylightNebula/DaylinMicroservices-Redis")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
